2caf39ead3ad7896f383e1cecd5015136cda23aa
mercury0114 <mlatinis@gmail.com>
2022-09-29 22:44:04 +0200

    Final simplifications

--- app/app/src/main/java/com/mercury0114/vocabulary/MainActivity.java
+++ app/app/src/main/java/com/mercury0114/vocabulary/MainActivity.java
@@ -2,6 +2,7 @@ package com.mercury0114.vocabulary;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mainTextView = (TextView)findViewById(R.id.mainTextView);
+        mainTextView.setText("A different text message\n");
     }
 }
