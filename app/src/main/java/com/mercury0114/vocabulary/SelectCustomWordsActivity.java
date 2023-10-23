bd780cdd3d4762fb2d831ba4519f8f2cb7f70fd6
mercury011 <m.latinis@gmail.com>
2023-10-24 00:36:44 +0200

    Add the dumb version of SelectCustomWordsActivity buttons

--- app/src/main/java/com/mercury0114/vocabulary/SelectCustomWordsActivity.java
+++ app/src/main/java/com/mercury0114/vocabulary/SelectCustomWordsActivity.java
@@ -2,10 +2,14 @@ package com.mercury0114.vocabulary;
 
 import static com.mercury0114.vocabulary.FilesReader.readLinesAndSort;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import com.mercury0114.vocabulary.QuestionAnswer.Column;
 import java.io.File;
 
 public class SelectCustomWordsActivity extends AppCompatActivity {
@@ -15,15 +19,40 @@ public class SelectCustomWordsActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.select_custom_content_layout);
     String filePath = getIntent().getStringExtra("PATH");
-    addButtonsToLinearLayout(filePath);
+    prepareStartButton(filePath);
+    addSelectWordButtons(filePath);
   }
 
-  private void addButtonsToLinearLayout(String filePath) {
+  private void prepareStartButton(String filePath) {
+    final Button startButton = (Button) findViewById(R.id.start_practice_button_id);
+    startButton.setOnClickListener(
+        new OnClickListener() {
+          public void onClick(View view) {
+            Intent intent = new Intent(SelectCustomWordsActivity.this, FileActivity.class);
+            // TODO(mariusl): implement proper logic
+            intent.putExtra("PATH", filePath);
+            intent.putExtra("COLUMN", Column.BOTH.name());
+            startActivity(intent);
+          }
+        });
+  }
+
+  private void addSelectWordButtons(String filePath) {
     LinearLayout linearLayout = (LinearLayout) findViewById(R.id.select_custom_content_id);
     for (String line : readLinesAndSort(new File(filePath))) {
-      Button button = new Button(this);
-      button.setText(line);
-      linearLayout.addView(button);
+      linearLayout.addView(createSelectButton(line));
     }
+  }
+
+  private Button createSelectButton(String line) {
+    Button button = new Button(this);
+    button.setText(line);
+    button.setOnClickListener(
+        new OnClickListener() {
+          public void onClick(View view) {
+            // TODO(mariusl): implement full logic
+          }
+        });
+    return button;
   }
 }
