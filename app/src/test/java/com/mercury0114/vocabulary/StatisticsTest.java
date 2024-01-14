c3b3b6ece2dc028e073bf85869474cf61430ebef
mercury011 <m.latinis@gmail.com>
2024-01-14 19:25:22 +0100

    Add updateStatistics tests

--- app/src/test/java/com/mercury0114/vocabulary/StatisticsTest.java
+++ app/src/test/java/com/mercury0114/vocabulary/StatisticsTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 import com.google.common.collect.ImmutableList;
+import com.mercury0114.vocabulary.QuestionAnswer.AnswerStatus;
+import java.util.NoSuchElementException;
 import org.junit.Test;
 
 public class StatisticsTest {
@@ -33,6 +35,15 @@ public class StatisticsTest {
         new Statistics(ImmutableList.of(createEntry("question1"), createEntry("question2")));
 
     assertEquals(statistics.getHardestQuestions(/* requestedNumber= */ 1).size(), 1);
+  }
+
+  @Test
+  public void updateOneStatisticsEntry_requestingToUpdateNonExistingEntry_throwsException() {
+    Statistics statistics = new Statistics(ImmutableList.of(createEntry("question")));
+
+    assertThrows(
+        NoSuchElementException.class,
+        () -> statistics.updateOneStatisticsEntry("non_existing_question", AnswerStatus.CORRECT));
   }
 
   private StatisticsEntry createEntry(String question) {
