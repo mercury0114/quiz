9abc994948d9b869376e992374c2d0ef5dfc8587
mercury011 <m.latinis@gmail.com>
2024-01-14 14:50:39 +0100

    Add main view to the vocabulary app

--- app/src/main/java/com/mercury0114/vocabulary/MainActivity.java
+++ app/src/main/java/com/mercury0114/vocabulary/MainActivity.java
@@ -4,6 +4,9 @@ import static com.mercury0114.vocabulary.FilesReader.VOCABULARY_PATH;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import java.io.File;
 
@@ -11,9 +14,18 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.main_layout);
     new File(VOCABULARY_PATH).mkdirs();
-    Intent intent = new Intent(MainActivity.this, FolderActivity.class);
-    intent.putExtra("PATH", VOCABULARY_PATH);
-    startActivity(intent);
+
+    Button button = findViewById(R.id.continue_button_id);
+    button.setOnClickListener(
+        new OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, FolderActivity.class);
+            intent.putExtra("PATH", VOCABULARY_PATH);
+            startActivity(intent);
+          }
+        });
   }
 }
